53beb1a74b7d1da06bd124bf82fb5c1e1d57e88b
BXimenaGomez123 <x10096444@gmail.com>
2025-04-08 10:49:27 -0500

    Lab7

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,27 +4,39 @@
 <p>Hello world</p>
 <img src="images/1689641970143.jpg" width="300" height="400"  alt="Yo">
 
+<script>
+import { onMount } from "svelte";
 
-{#await fetch("https://api.github.com/users/BXimenaGomez123")}
-  <p>Loading...</p>
-{:then response}
-  {#await response.json()}
-    <p>Decoding...</p>
-  {:then data} 
+let githubData = null;
+let loading = true;
+let error = null;
+
+onMount(async () => {
+    try {
+        const response = await fetch("https://api.github.com/users/BXimenaGomez123");
+        githubData = await response.json();
+    } catch (err) {
+        error = err;
+    }
+    loading = false;
+});
+
+</script>
+
+{#if loading}
+    <p>Loading...</p>
+{:else if error}
+    <p class="error">Something went wrong: {error.message}</p>
+{:else}
     <section>
-      <h2>My Github Stats</h2>
-      <dl>
-        <dt>Followers</dt>
-        <dd>{data.followers}</dd>
-        <dt>Following</dt>
-        <dd>{data.following}</dd>
-        <dt>Public Repos</dt>
-        <dd>{data.public_repos}</dd> 
-      </dl>
-    </section>  
-  {:catch error}
-    <p class="error">Something went wrong: {error.message}</p>
-  {/await}
-  {:catch error}
-    <p class="error">Something went wrong: {error.message}</p>
-{/await}
+        <h2>My GitHub Stats</h2>
+        <dl>
+            <dt>Followers</dt>
+            <dd>{githubData.followers}</dd>
+            <dt>Following</dt>
+            <dd>{githubData.following}</dd>
+            <dt>Public Repositories</dt>
+            <dd>{githubData.public_repos}</dd>
+        </dl>
+    </section>
+{/if}
